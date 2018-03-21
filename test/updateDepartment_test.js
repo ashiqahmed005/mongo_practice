306304d7a9dfd6f68f40e57dab6b24cc3db52d97
ashiqahmed005 <ashiqahmed005@gmail.com>
2018-03-21 14:51:35 +0200

    update test function

--- test/updateDepartment_test.js
+++ test/updateDepartment_test.js
@@ -8,28 +8,33 @@ describe('Updating the department', () => {
         .then(() => done())
     });
     
-    
-    it('Instance of Department set and save', (done) => {
-        IT.set('name', 'Accounting')
-        IT.save()
-        .then(()=> Department.find({name: 'Accounting'}))
+    function assertname(operation, done){
+        operation
+        .then(()=> Department.find({}))
         .then((departments) =>{
             console.log(departments[0]._id);
             //console.log(IT._id);
             assert(departments[0].name === 'Accounting');
             done();
         });
+
+    }
+    it('Instance of Department set and save', (done) => {
+        IT.set('name', 'Accounting')
+        assertname(IT.save(), done);
         
     });  
 
-    // it('find the department', (done) =>{
-    //     Department.find({name: 'IT'})
-    //     .then((departments) =>{
-    //         //console.log(departments[0]._id);
-    //         //console.log(IT._id);
-    //         assert(departments[0]._id.toString() === IT._id.toString());
-    //         done();
-    //     });
-        
-    // });
+    it('instance of department by using update ', (done) =>{
+      assertname(IT.update({name: 'Accounting'}), done);
+    });
+    it('update all department by using class update and find criteria', (done) =>{
+        assertname(Department.update({name: 'IT'},{name: 'Accounting'}), done);
+    });
+    it('update one department by using findById class update', (done) =>{
+        assertname(Department.findByIdAndUpdate(IT._id,{name: 'Accounting'}), done);
+    });
+    it('update one department by using findOneAndUpdate class update', (done) =>{
+        assertname(Department.findOneAndUpdate({name: 'IT'},{name: 'Accounting'}), done);
+    });   
 });
